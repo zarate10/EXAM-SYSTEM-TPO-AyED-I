ff9420a13198ac27fbd60422a8f14ab38245a14c
zarate10 <zaratelautaro999@gmail.com>
2022-11-12 13:41:04 -0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#################################################
-##                                             ##
-##           TRABAJO PRÁCTICO FINAL            ##
-##     ALGORITMOS Y ESTRUCTURA DE DATOS I      ##
-##                                             ##
-##     Cañete, Andrés                          ##
-##     Ciardelli, Martín                       ##
-##     Touris, Santiago                        ##
-##     Traba, Federico                         ##
-##     Zarate, Lautaro                         ##
-##                                             ##
-#################################################
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+#  Copyright 2022 by Diógenes y los perros
+#  Equipo: Cañete Andrés, Ciardelli Martín, Touris Santiago, Traba Federico, Zarate Lautaro
+
+import os
+
+# módulos propios
+import registro
+
+if __name__ == '__main__': 
+    registro
---
+++ registro.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+#  registro.py
+#  
+#  Copyright 2022 by Diógenes y los perros
+#  Equipo: Cañete Andrés, Ciardelli Martín, Touris Santiago, Traba Federico, Zarate Lautaro
+
+import os 
+
+usuarios = os.scandir('./db/usuarios')
+
+def usuario_existe(): 
+    new_user = input('Ingrese un nombre de usuario: ')
+    new_user += '.txt'
+
+    for user in usuarios: 
+        
+        if user.name == new_user: 
+            pass 
+
+usuario_existe()
