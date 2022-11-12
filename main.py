5928117c59622e103a45f0829c5cbfb07a083b90
zarate10 <zaratelautaro999@gmail.com>
2022-11-12 13:02:12 -0300

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+#################################################
+##                                             ##
+##           TRABAJO PRÁCTICO FINAL            ##
+##     ALGORITMOS Y ESTRUCTURA DE DATOS I      ##
+##                                             ##
+##     Cañete, Andrés                          ##
+##     Ciardelli, Martín                       ##
+##     Touris, Santiago                        ##
+##     Traba, Federico                         ##
+##     Zarate, Lautaro                         ##
+##                                             ##
+#################################################
